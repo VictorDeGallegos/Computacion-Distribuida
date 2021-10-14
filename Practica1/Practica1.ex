ae720b0e2336d5371fec7795bea475b3b3a05ca5
Victor Gallegos <316160456@ciencias.unam.mx>
2021-10-13 19:17:37 -0500

    Se realizo el Primer Modulo

---
+++ Practica1/Practica1.ex
@@ -0,0 +1,64 @@
+defmodule Module1 do
+  @moduledoc """
+  Calculo de Funciones básicas:
+     fibonaccionacci
+	   factorial
+     random_probability
+  """
+  # Fibonacci
+  # Ejemplo de Ejecucion: Module1.fibonacci(10)
+  def fibonacci(0) do
+    0
+  end
+
+  def fibonacci(1) do
+    1
+  end
+
+  def fibonacci(n) do
+    fibonacci(n-1) + fibonacci(n-2)
+  end
+
+ # Factorial
+ # Ejemplo de Ejecucion Module1.factorial(5)
+
+  def factorial(n) when n <= 1, do: 1
+  def factorial(n) when n > 1, do: n * factorial(n - 1)
+
+  # Random Probability
+  # Ejemplo de Ejecucion Module1.random_probability(10)
+  def random_probability(n) do
+    #Dado un número n, escoger un número aleatorio en el rango [1, n], digamos k
+    #y determinar cuál es la probabilidad de que salga un número aleatorio
+    #entre [k, n], el chiste obtener el número aleatorio.
+    k = :rand.uniform(n)
+    IO.puts("[#{k},...,#{n}]")
+    resultado = (n-k+1)/n
+    "La Probabilidad es de:, #{resultado}"
+  end
+
+  def digits(n) do
+    n
+  end
+
+end
+
+
+defmodule Module3 do
+
+  def rev(l) do
+    :ok
+  end
+
+  def sieve_of_erathostenes(n) do
+    :ok
+  end
+
+end
+
+defmodule Module4 do
+
+  def monstructure() do
+    :ok
+  end
+end
